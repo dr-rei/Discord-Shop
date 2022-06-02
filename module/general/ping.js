8717fba5a80f1625a57447a3692dae1bd87d85e3
WarlordXZ <sebastian.reinard.halim@gmail.com>
2022-06-02 10:47:38 +0700

    move and update ping command!

---
+++ module/general/ping.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+		
+	async execute(interaction, config) {
+
+		const permissions = require('../permission.js')
+		const denied = require('../../function/permissionDenied.js')
+		const allowed = await permissions.slashRolePermission(config.permission.admin, interaction)
+		if(!allowed){
+			denied.permissionDenied(interaction)
+		}else{
+			
+			const respone = new MessageEmbed()
+				.setColor("#0000ff")
+				.setDescription("***CALCULATING PING....***");
+
+		
+			const reply = await interaction.reply({ embeds: [respone], fetchReply: true })
+			const PingAPI = interaction.client.ws.ping
+			const PingBOT = reply.createdTimestamp - interaction.createdTimestamp 
+	
+			
+			const result = new MessageEmbed()
+				.setColor("#0000ff")
+				.setDescription("**PING RESULT**")
+				.addFields(
+					{ name: 'API Ping :', value: "```"+PingAPI+" ms```", inline: true },
+					{ name: 'Bot Ping :', value: "```"+PingBOT+" ms```", inline: true },
+					
+				)
+			await interaction.editReply({embeds: [result]})
+
+		}
+		
+		
+	},
+};
